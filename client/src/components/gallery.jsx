7aca37270afd09e15a883d6feead8c7185926321
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2021-10-02 11:47:39 +0530

    add dedupe logic to gallery

--- client/src/components/gallery.jsx
+++ client/src/components/gallery.jsx
@@ -3,138 +3,147 @@ import React, { Component } from 'react';
 import { s3BucketName } from './../config.json'
 
 class menusSection extends Component {
+
+    constructor() {
+        super()
+
+        this.images = []
+    }
+
     state = {
         images: [
             {
-                image: s3BucketName + '1.jpg',
-                description: 'Polas ceil view'
+                image: `${s3BucketName}/1.jpg`,
+                description: 'Polas ceil view',
+                selected: false
             }, {
-                image: s3BucketName + '2.jpg',
-                description: 'Polas top view'
+                image: `${s3BucketName}/2.jpg`,
+                description: 'Polas top view',
+                selected: false
             }, {
-                image: s3BucketName + '3.jpg',
-                description: 'Cycling man architecture'
+                image: `${s3BucketName}/3.jpg`,
+                description: 'Cycling man architecture',
+                selected: false
             }, {
-                image: s3BucketName + '4.jpg',
-                description: 'Polas top road view dining'
+                image: `${s3BucketName}/4.jpg`,
+                description: 'Polas top road view dining',
+                selected: false
             }, {
-                image: s3BucketName + '5.jpg',
-                description: 'Colourful cups'
+                image: `${s3BucketName}/5.jpg`,
+                description: 'Colourful cups',
+                selected: false
             }, {
-                image: s3BucketName + '6.jpg',
-                description: 'Polas ship themed house'
+                image: `${s3BucketName}/6.jpg`,
+                description: 'Polas ship themed house',
+                selected: false
             }, {
-                image: s3BucketName + '7.jpg',
-                description: 'Lemon tea'
+                image: `${s3BucketName}/7.jpg`,
+                description: 'Lemon tea',
+                selected: false
             }, {
-                image: s3BucketName + '8.jpg',
-                description: 'Sizzling brownie'
+                image: `${s3BucketName}/8.jpg`,
+                description: 'Sizzling brownie',
+                selected: false
             }, {
-                image: s3BucketName + '9.jpg',
-                description: 'Dhaba Cafe'
+                image: `${s3BucketName}/9.jpg`,
+                description: 'Dhaba Cafe',
+                selected: false
             }, {
-                image: s3BucketName + '10.jpg',
-                description: 'Nirvana'
+                image: `${s3BucketName}/10.jpg`,
+                description: 'Nirvana',
+                selected: false
             }, {
-                image: s3BucketName + '11.jpg',
-                description: 'Taste of Rajwada'
+                image: `${s3BucketName}/11.jpg`,
+                description: 'Taste of Rajwada',
+                selected: false
             }, {
-                image: s3BucketName + '12.jpg',
-                description: 'Thancos natural icecream'
+                image: `${s3BucketName}/12.jpg`,
+                description: 'Thancos natural icecream',
+                selected: false
             }, {
-                image: s3BucketName + '13.jpg',
-                description: 'U.S. Pizza'
+                image: `${s3BucketName}/13.jpg`,
+                description: 'U.S. Pizza',
+                selected: false
             }, {
-                image: s3BucketName + '14.jpg',
-                description: 'Ship themed room inner view'
+                image: `${s3BucketName}/14.jpg`,
+                description: 'Ship themed room inner view',
+                selected: false
             }, {
-                image: s3BucketName + '15.jpg',
-                description: 'Pola view from roof'
+                image: `${s3BucketName}/15.jpg`,
+                description: 'Pola view from roof',
+                selected: false
             }, {
-                image: s3BucketName + '16.jpg',
-                description: 'U.S. Pizza'
+                image: `${s3BucketName}/16.jpg`,
+                description: 'U.S. Pizza',
+                selected: false
             }, {
-                image: s3BucketName + '17.jpg',
-                description: 'Oreo icecreame'
+                image: `${s3BucketName}/17.jpg`,
+                description: 'Oreo icecreame',
+                selected: false
             }, {
-                image: s3BucketName + '18.jpg',
-                description: 'U.S. Pizza'
+                image: `${s3BucketName}/18.jpg`,
+                description: 'U.S. Pizza',
+                selected: false
             }, {
-                image: s3BucketName + '19.jpg',
-                description: 'Polas seating arrangement'
+                image: `${s3BucketName}/19.jpg`,
+                description: 'Polas seating arrangement',
+                selected: false
             }, {
-                image: s3BucketName + '20.jpg',
-                description: 'Product show at Polas'
+                image: `${s3BucketName}/20.jpg`,
+                description: 'Product show at Polas',
+                selected: false
             }, {
-                image: s3BucketName + '21.jpg',
-                description: 'Ship themed house'
+                image: `${s3BucketName}/21.jpg`,
+                description: 'Ship themed house',
+                selected: false
             }, {
-                image: s3BucketName + '22.jpg',
-                description: 'Empty space for parties'
+                image: `${s3BucketName}/22.jpg`,
+                description: 'Empty space for parties',
+                selected: false
             },
         ],
     }
 
-    getRandomImage() {
-        const arr = this.state.images;
-        const randomIndex = Math.floor(Math.random() * arr.length);
+    componentDidMount() {
+        const images = JSON.parse(JSON.stringify(this.state.images))
 
-        return arr[randomIndex];
+        for(let i=0; i<8; i++) {
+            let randomIndex = this.getRandomIndex()
+            
+            while (images[randomIndex].selected) {
+                randomIndex = this.getRandomIndex()
+            }
+
+            this.images[i] = images[randomIndex]
+
+            images[randomIndex].selected = true
+            this.setState({ images })
+        }
+
+    }
+
+    getRandomIndex() {
+        const { images } = this.state
+        
+        const randomIndex = Math.floor(Math.random() * images.length)
+
+        return randomIndex
     }
 
     render() {
-        const img1 = this.getRandomImage(), img2 = this.getRandomImage(), img3 = this.getRandomImage(), img4 = this.getRandomImage(), img5 = this.getRandomImage(), 
-        img6 = this.getRandomImage(), img7 = this.getRandomImage(), img8 = this.getRandomImage();
-
         return ( 
-            <React.Fragment>
-                <section className="section-menus" id="gallery">
-                    <ul className="menus-showcase">
-                        <li>
+            <section className="section-menus" id="gallery">
+                <ul className="menus-showcase">
+                    {this.images.map((img, key) =>
+                        <li key={key}>
                             <figure className="menu-photo">
-                                <img src={img1.image} alt={img1.description} />
+                                <img src={img.image} alt={img.description} />
                             </figure>
                         </li>
-                        <li>
-                            <figure className="menu-photo">
-                                <img src={img2.image} alt={img2.description} />
-                            </figure>
-                        </li>
-                        <li>
-                            <figure className="menu-photo">
-                                <img src={img3.image} alt={img3.description} />
-                            </figure>
-                        </li>
-                        <li>
-                            <figure className="menu-photo">
-                                <img src={img4.image} alt={img4.description} />
-                            </figure>
-                        </li>
-                    </ul>
-                    <ul className="menus-showcase">
-                        <li>
-                            <figure className="menu-photo">
-                                <img src={img5.image} alt={img5.description} />
-                            </figure>
-                        </li>
-                        <li>
-                            <figure className="menu-photo">
-                                <img src={img6.image} alt={img6.description} />
-                            </figure>
-                        </li>
-                        <li>
-                            <figure className="menu-photo">
-                                <img src={img7.image} alt={img7.description} />
-                            </figure>
-                        </li>
-                        <li>
-                            <figure className="menu-photo">
-                                <img src={img8.image} alt={img8.description} />
-                            </figure>
-                        </li>
-                    </ul>
-                </section>
-            </React.Fragment>
+                    )}
+                    
+                </ul>
+            </section>
         );
     }
 }
